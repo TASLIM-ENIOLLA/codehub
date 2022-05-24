924d994026f279926e29f48b1adf25f28b719890
TASLIM-ENIOLLA <61813017+TASLIM-ENIOLLA@users.noreply.github.com>
2022-05-24 16:23:35 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default ({Component, pageProps}) => {
                 <meta httpEquiv="x-ua-compatible" content="ie=edge" />
                 <meta name="description" content="" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                <title>Business | Bootstrap 5 Business Template</title>
+                <title>CodeHub Technological Institute</title>
             </Head>
             <Component className = "po-rel" style = {{zIndex: 0}} {...pageProps} />
             <Script src="assets/js/bootstrap.bundle.min.js"></Script>
